3b1c42c351a563fb142915ab756a6f2f2ec2907f
Esteban Nuñez <ghostofthao1988@gmail.com>
2023-05-19 17:46:28 -0300

    Update Alumno.java

--- src/entidades/Alumno.java
+++ src/entidades/Alumno.java
@@ -5,10 +5,95 @@
  */
 package entidades;
 
+import java.sql.Date;
+
 /**
  *
  * @author nacho
  */
 public class Alumno {
     
+    private int id_alumno;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private Date fechaNacimiento;
+    private boolean estado;
+    
+    // constructor vacio
+    public Alumno() {
+    }
+    
+    // constructor sin id
+    public Alumno(String dni, String nombre, String apellido, Date fechaNacimiento, boolean estado) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.estado = estado;
+    }
+    
+    // constructor con id
+    public Alumno(int id_alumno, String dni, String nombre, String apellido, Date fechaNacimiento, boolean estado) {
+        this.id_alumno = id_alumno;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.estado = estado;
+    }
+
+    public int getId_alumno() {
+        return id_alumno;
+    }
+
+    public void setId_alumno(int id_alumno) {
+        this.id_alumno = id_alumno;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return  dni + ", " + nombre + ", " + apellido;
+    }
+       
 }
