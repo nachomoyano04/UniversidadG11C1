affbeb288303944f5f449ca23fecccb8c1276715
Nacho <nachomoyag@gmail.com>
2023-05-19 17:56:11 -0300

    inscripcion y materia (entidades)

--- src/entidades/Inscripcion.java
+++ src/entidades/Inscripcion.java
@@ -10,5 +10,69 @@ package entidades;
  * @author nacho
  */
 public class Inscripcion {
+    //atributos inscripcion
+    private int id_inscripcion; 
+    private Alumno alumno; 
+    private Materia materia; 
+    private double nota;
+    
+    //3 constructores
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(Alumno alumno, Materia materia, double nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripcion(int id_inscripcion, Alumno alumno, Materia materia, double nota) {
+        this.id_inscripcion = id_inscripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+    
+    //getters and setters
+
+    public int getId_inscripcion() {
+        return id_inscripcion;
+    }
+
+    public void setId_inscripcion(int id_inscripcion) {
+        this.id_inscripcion = id_inscripcion;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    //override del metodo to String para pronta utilizacion en vistas
+    @Override
+    public String toString() {
+        return alumno + " " + materia + " " + nota;
+    }
+    
     
 }
--- src/entidades/Materia.java
+++ src/entidades/Materia.java
@@ -1,14 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entidades;
 
-/**
- *
- * @author nacho
- */
 public class Materia {
+    //atributos materia
+    private int id_materia;
+    private String nombre;
+    private int año;
+    private boolean estado;
+    
+    // 3 constructores
+    public Materia() {
+    }
+
+    public Materia(int id_materia, String nombre, int año, boolean estado) {
+        this.id_materia = id_materia;
+        this.nombre = nombre;
+        this.año = año;
+        this.estado = estado;
+    }
+
+    public Materia(String nombre, int año, boolean estado) {
+        this.nombre = nombre;
+        this.año = año;
+        this.estado = estado;
+    }
+    
+    //metodos getter y settters
+
+    public int getId_materia() {
+        return id_materia;
+    }
+
+    public void setId_materia(int id_materia) {
+        this.id_materia = id_materia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    //modificamos metodo toString para pronta utilizacion en vistas
+
+    @Override
+    public String toString() {
+        return  nombre + " " + año + " " + estado;
+    }
     
 }
